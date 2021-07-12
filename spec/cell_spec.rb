b8fb9b511f422ea49678b71dd7a318332d910800
Weston Ellis <ellisweston112@gmail.com>
2021-07-12 17:56:38 -0600

    Create method empty and start on place ship

---
+++ spec/cell_spec.rb
@@ -0,0 +1,43 @@
+require './lib/ship'
+require './lib/cell'
+
+
+RSpec.describe Cell do
+
+  context 'initialize' do
+    it 'exits' do
+      cell = Cell.new('B4')
+
+      expect(cell).to be_a(Cell)
+    end
+
+    it 'can take a coordinate' do
+      cell = Cell.new('B4')
+
+      expect(cell.coordinate).to eq('B4')
+    end
+  end
+
+  context 'methods' do
+
+    it 'can place a ship' do
+      cell = Cell.new('B4')
+      cruiser = Ship.new('Cruiser', 3)
+
+      expect(cell.ship).to eq(nil)
+      expect(cell.empty?).to eq(true)
+
+      cell.place_ship('Cruiser')
+
+      expect(cell.empty?).to eq(false)
+      expect(cell.ship).to eq(cruiser)
+    end
+
+  end
+
+
+
+
+
+
+end
