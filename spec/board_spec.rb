3f80717bdfdf3ec9a301811a957f3137ddafa7f8
Rteske <77249542+Rteske@users.noreply.github.com>
2021-07-14 18:27:07 -0700

    Add valid_row with test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,6 @@
 require './lib/board'
 require './lib/ship'
+require 'pry'
 
 RSpec.describe Board do
   context 'initialize' do
@@ -38,21 +39,35 @@ RSpec.describe Board do
         expect(board.valid_coordinate?('A22')).to eq(false)
       end
 
-      it 'can validate ship placement' do
-        board = Board.new
-        cruiser = Ship.new('Cruiser', 3)
-        submarine = Ship.new('Submarine', 2)
+      context 'can validate ship placement' do
+        it "can validate a row placement" do
+          board = Board.new
+          cruiser = Ship.new('Cruiser', 3)
+          submarine = Ship.new('Submarine', 2)
 
-        board.cells
-        #
-        expect(board.valid_placement?(cruiser, ['A1', 'A2'])).to eq(false)
-        expect(board.valid_placement?(submarine, ['A2', 'A3', 'A4'])).to eq(false)
-        #require 'pry'; binding.pry
-        expect(board.valid_placement?(cruiser, ['A1', 'A2', 'A4'])).to eq(false)
-        expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
-        expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
-        expect(board.valid_placement?(cruiser, ['A1', 'A2', 'A3'])).to eq(true)
-        expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+          board.cells
+          expect(board.valid_placement?(cruiser, ['A1', 'A2'])).to eq(false)
+          expect(board.valid_placement?(submarine, ['A2', 'A3', 'A4'])).to eq(false)
+          expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+          expect(board.valid_placement?(cruiser, ['A1', 'A2', 'A4'])).to eq(false)
+          expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+          expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+          expect(board.valid_placement?(cruiser, ['A1', 'A2', 'A3'])).to eq(true)
+        end
+        it 'can validate a column placement' do
+          board = Board.new
+          cruiser = Ship.new('Cruiser', 3)
+          submarine = Ship.new('Submarine', 2)
+
+          expect(board.valid_placement?(submarine, ["D1", "C1"])).to eq(false)
+          expect(board.valid_placement?(submarine, ["C2", "B2"])).to eq(false)
+          expect(board.valid_placement?(cruiser, ['C1', 'B1', 'A1'])).to eq(false)
+          expect(board.valid_placement?(cruiser, ['B2', 'B2', 'B2'])).to eq(false)
+          expect(board.valid_placement?(submarine, ['A2', 'B2'])).to eq(true)
+          expect(board.valid_placement?(cruiser, ['A4', 'B4', 'C4'])).to eq(true)
+
+        end
+
       end
 
   end
